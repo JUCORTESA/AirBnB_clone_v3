ac0a7809a482dabc70cf2047dc899ee153542256
= <=>
2020-01-27 21:29:52 +0000

    [fix] return a dictionary not a json.dumps element

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,10 +2,9 @@
 """Creationg route for Blueprint
 """
 from api.v1.views import app_views
-import json
 
 
 @app_views.route('/status')
 def response():
     dic = {"status": "OK"}
-    return json.dumps(dic)
+    return dic
