a48574fff6266872ec079da411b3010da6866a77
jucortesa <984@holbertonschool.com>
2020-01-27 22:23:11 +0000

    [update] add count of objects and jsonify

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,21 @@
 """Creationg route for Blueprint
 """
 from api.v1.views import app_views
-
+from models import storage
+from flask import jsonify
 
 @app_views.route('/status')
 def response():
     dic = {"status": "OK"}
-    return dic
+    return jsonify(dic)
+
+@app_views.route('/stats')
+def class_counter():
+    dic = {}
+    dic["amenities"] = storage.count("Amenity")
+    dic["cities"] = storage.count("City")
+    dic["places"] = storage.count("Place")
+    dic["reviews"] = storage.count("Review")
+    dic["states"] = storage.count("State")
+    dic["users"] = storage.count("User")
+    return jsonify(dic)
