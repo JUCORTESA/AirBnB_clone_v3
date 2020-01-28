3b6951ece58fa1ce22c8e73795c9f4b463f6a558
= <=>
2020-01-28 19:17:22 +0000

    [update] PUT DELETE POST GET methods

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,11 +3,12 @@
 """
 
 from models import storage
+from models.state import State
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 
 
-@app_views.route('/states/', methods=['GET'])
+@app_views.route('/states', methods=['GET'])
 def list_dict():
     """ list of objetc in dict form
     """
@@ -35,3 +36,40 @@ def state_id(state_id):
                 storage.save()
                 return (jsonify(aux))
         abort(404)
+
+
+@app_views.route('/states', methods=['POST'])
+def add_item():
+    """ add a new item
+    """
+    if not request.json:
+        return jsonify("Not a JSON"), 400
+    else:
+        content = request.get_json()
+        if "name" not in content.keys():
+            return jsonify("Missing name"), 400
+        else:
+            new_state = State(**content)
+            new_state.save()
+            print(new_state)
+            return (jsonify(new_state.to_dict()), 201)
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'])
+def update_item(state_id):
+    """ update item
+    """
+    dic = storage.all("State")
+    for key in dic:
+        if dic[key].id == state_id:
+            if not request.json:
+                return jsonify("Not a JSON"), 400
+            else:
+                forbidden = ["id", "update_at", "created_at"]
+                content = request.get_json()
+                for k in content:
+                    if k not in forbidden:
+                        setattr(dic[key], k, content[k])
+                dic[key].save()
+                return(jsonify(dic[key].to_dict()))
+    abort(404)
